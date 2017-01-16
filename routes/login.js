8b6f9d2596d4ca31343d62cc3286f0cfe72623f4
bijh <bijh@uyunsoft.cn>
2017-01-16 21:56:02 +0800

    pretty javascript

--- routes/login.js
+++ routes/login.js
@@ -3,9 +3,11 @@ var process = require('process');
 var events = require('events');
 var util = require('util');
 /* GET login page. */
-module.exports = function (app) {
+module.exports = function(app) {
   app.get('/', function(req, res, next) {
-    res.render('login', { name: 'nick' });
+    res.render('login', {
+      name: 'nick'
+    });
     // process.nextTick(function () {
     //   setTimeout(function () {
     //     console.log('TimeOut');
@@ -22,7 +24,7 @@ module.exports = function (app) {
     // pro.on('data', function (data) { console.log(data) });
     // pro.emit('data', '事件触发');
   });
-  app.post('/login', function (req, res) {
+  app.post('/login', function(req, res) {
     res.send(200);
   });
 };
