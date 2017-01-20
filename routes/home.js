fa59d915e627d4a665c3efe0b0e5d781a484e88c
shimmer.bjh <shimmer.bjh@ip-v6.local>
2017-01-20 11:10:05 +0800

    home commoditys views

--- routes/home.js
+++ routes/home.js
@@ -14,7 +14,15 @@ module.exports = function (app) {
       //     res.render('home', { commoditys: commoditys });
       //   }
       // })
-      res.render('home');
+      var commoditys = [],
+          Commodity = global.dbHelper.getModel('commodity');
+      Commodity.find({}, function(err, doc){
+        if (err) return res.send(500, { error: err });
+        if (doc) {
+          commoditys = doc;
+          res.render('home', {commoditys: commoditys});
+        }
+      })
     });
   app.route('/addcommodity')
     .get(function (req, res) {
@@ -23,14 +31,6 @@ module.exports = function (app) {
     .post(function (req, res) {
       var query = { name: req.body.name };
       var Commodity = global.dbHelper.getModel('commodity');
-      // Commodity.findOne({ name: newCom.name }, function (error, doc) {
-      //   if (error) {
-      //     res.sendStatus(500);
-      //     console.log('error');
-      //   } else if (doc) {
-
-      //   }
-      // })
       Commodity.findOneAndUpdate(query, req.body, { upsert: true }, function (err, doc) {
         if (err) return res.send(500, { error: err });
         return res.send("succesfully saved");
